3067767f18619236e38316cbefc5a1e80efcfd15
josephyu12 <82769846+josephyu12@users.noreply.github.com>
2025-07-23 15:52:59 -0400

    changing cors

--- server/main.py
+++ server/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://claude-chatbot-one.vercel.app/"],
+    allow_origins=["http://localhost:3000", "https://claude-chatbot-one.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
